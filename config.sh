26c465b0d7807af579eee535a2424aafda9e99f0
Matthew Brett <matthew.brett@gmail.com>
2018-12-01 16:43:02 +0000

    Test inside source directory

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pytest --pyargs cftime
+    pytest -v ../cftime
 }
