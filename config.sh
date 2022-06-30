c162301c97e450c32342bccfc37892613ed4f714
Jeff Whitaker <jswhit@fastmail.fm>
2022-06-29 19:30:57 -0600

    update

--- config.sh
+++ config.sh
@@ -3,6 +3,5 @@
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pip install pytest pytest-cov
     pytest -v ../cftime/test/test_cftime.py
 }
