3075a933e54df8d4b5a8cca5928e1ece0d02ffba
Jeff Whitaker <jswhit@fastmail.fm>
2022-06-29 19:13:38 -0600

    install pytest in run_test

--- config.sh
+++ config.sh
@@ -3,5 +3,6 @@
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
+    pip install pytest pytest-cov
     pytest -v ../cftime/test/test_cftime.py
 }
